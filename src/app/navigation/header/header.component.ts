157a79610018cca776a73fd9509b8ea94ad21d12
LAPTOP-LBD22PNR\Sarwar <ohailhayat777@gmail.com>
2021-11-24 23:39:39 +0530

    initial commit

---
+++ src/app/navigation/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+
+  ngOnInit(): void {
+
+   this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus
+    })
+  }
+
+  onToggleSidenav(){
+     this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey:'AIzaSyDFqLHcyB_IH9s-N1HiT8n1aVV61jIwlCQ'
+};
